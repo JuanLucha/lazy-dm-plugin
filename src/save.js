93180f4213ba08313563999d53041c1bb1fc3d4d
Juan Adolfo Lucha García <juanlucha@gmail.com>
2022-07-20 18:48:44 +0200

    Edit basic template

--- src/save.js
+++ src/save.js
@@ -3,7 +3,7 @@
  *
  * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
  */
-import { __ } from '@wordpress/i18n';
+import { __ } from "@wordpress/i18n";
 
 /**
  * React hook that is used to mark the block wrapper element.
@@ -11,7 +11,7 @@ import { __ } from '@wordpress/i18n';
  *
  * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { InnerBlocks, useBlockProps } from "@wordpress/block-editor";
 
 /**
  * The save function defines the way in which the different attributes should
@@ -23,12 +23,11 @@ import { useBlockProps } from '@wordpress/block-editor';
  * @return {WPElement} Element to render.
  */
 export default function save() {
+	const saveinfo = useBlockProps.save();
+	console.log("pepe", saveinfo);
 	return (
-		<p { ...useBlockProps.save() }>
-			{ __(
-				'Lazy DM Plugin – hello from the saved content!',
-				'lazy-dm-plugin'
-			) }
-		</p>
+		<span {...saveinfo}>
+			<InnerBlocks.Content />
+		</span>
 	);
 }
